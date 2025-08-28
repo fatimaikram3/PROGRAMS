6ea3e21c65795499fdc9bc28e80fed6a00941988
Fatima Ikram <fatimaikram3127@gmail.com>
2025-08-28 10:52:32 -0700

    Add files via upload

---
+++ conditions1.py.py
@@ -0,0 +1,13 @@
+#if else
+age=18
+if(age>=18):
+    print("you are an adult")
+else:
+    print("you are not an adult")
+
+#number is even or odd
+number=input("Enter a number:")
+if("number%2==0"):
+    print("The number is even")
+else:
+    print("The number is odd")
