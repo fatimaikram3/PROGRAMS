2fb97f7a75d92ae869a1b4f9e38c18ab1f139c7f
Fatima Ikram <fatimaikram3127@gmail.com>
2025-08-28 10:54:04 -0700

    Add files via upload

---
+++ conditions2.py
@@ -0,0 +1,27 @@
+#if elif else (syntax)
+
+#if():
+#   statement
+#elif():
+#   statement
+#else():
+#   statement
+
+light="green"
+if("light=red"):
+    print("stop")
+elif("light=yellow"):
+    print("look")
+else :
+    print("go")
+
+#nested if
+age=34
+if(age>=18):
+    if(age>=80):
+        print("cannot drive")
+    else:
+        print("can drive")
+else:
+    print("cannot drive")
+
